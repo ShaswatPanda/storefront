661e09a7941b83bfc5bc8dc484079e056690032d
ShaswatPanda <panda.shaswat@gmail.com>
2022-05-27 18:32:01 +0530

    Initial commit

---
+++ playground/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+# Request -> Response
+# Request handler
+# Action
+
+def say_hello(request):
+    # Pull data from db and transform
+    return HttpResponse('Hello World!')
