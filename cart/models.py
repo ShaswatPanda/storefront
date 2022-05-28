928e3dac0f6000cbe23cac58155309844003cd5b
ShaswatPanda <panda.shaswat@gmail.com>
2022-05-28 13:48:08 +0530

    Added app models

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Cart(models.Model):
+    items = models.TextField()
+    item_quantities = models.TextField()
+    coupons = models.TextField()
+    grand_total = models.TextField()
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Order(models.Model):
+    time_placed = models.TextField()
+    billing_address = models.TextField()
+    shipping_address = models.TextField()
+    mode_of_payment_used = models.TextField()
+    grand_total = models.TextField()
